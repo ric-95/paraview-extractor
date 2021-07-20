0efb59e179d85a7eb67b8f243d3337d22f332378
Ricardo Franco <ricardofranco95e@gmail.com>
2021-07-19 22:08:35 -0500

    implemented parametrized pipeline

--- bin/two_point_correlation_pipeline.py
+++ bin/two_point_correlation_pipeline.py
@@ -1,6 +1,7 @@
 import json
 from string import Template
 import numpy as np
+import os
 
 
 def parse_args():
@@ -21,18 +22,62 @@ def parse_args():
     parser.add_argument("--line-resolution", type=int, help="Resolution of "
                                                             "extracted lines.")
     parser.add_argument("--output-dir", type=str, help="Directory to write"
-                                                       "csv dump.")
+                                                       "json dump.")
     return parser.parse_args()
 
 
-FILE_TEMPLATE = Template("LINE_${number}_T=${timestep}.csv")
-TARGET_DICT = {"lines": [{}]}
+FILE_TEMPLATE = Template("LINE${number}_T=${timestep}.csv")
+VARIABLES = ("U", "UMean")
 PI = np.pi
-def create_line_definition(point_1, )
+
+
+def create_line_definition(point1, point2, resolution, timestep, variables,
+                           output):
+    return {"point1": point1, "point2": point2,
+            "resolution": resolution, "timestep": timestep,
+            "variables": variables, "output": output}
+
+
+def transform_cylindrical_to_cartesian(radial, axial, theta):
+    """Assumes that axial direction coincides with the z-axis"""
+    x = radial*np.cos(theta)
+    y = radial*np.sin(theta)
+    z = axial
+    return [x, y, z]
+
+
+def dump_to_json(obj, file, **kwargs):
+    with open(file, "w") as f:
+        f.write(json.dumps(obj, **kwargs))
+
+
+def run(point1, point2, lines_per_timestep,
+        line_resolution, timesteps, output_dir):
+    radial1, axial1 = point1
+    radial2, axial2 = point2
+    thetas = np.arange(lines_per_timestep) * 2 * PI / lines_per_timestep
+    point1s = [transform_cylindrical_to_cartesian(radial1, axial1, theta)
+               for theta in thetas]
+    point2s = [transform_cylindrical_to_cartesian(radial2, axial2, theta)
+               for theta in thetas]
+    outputs_list = [[FILE_TEMPLATE.substitute(number=str(line_num).zfill(3),
+                                              timestep=timestep)
+                     for line_num in range(len(thetas))]
+                    for timestep in timesteps]
+    lines = [create_line_definition(point1, point2, line_resolution,
+                                    timestep, VARIABLES, output)
+             for timestep, outputs in zip(timesteps,
+                                          outputs_list)
+             for point1, point2, output in zip(point1s, point2s, outputs)
+             ]
+    path = os.path.join(output_dir, "two-point-corr-extraction.json")
+    dump_to_json({"lines": lines}, path, indent=4)
+
 
 def main():
     args = parse_args()
-
+    run(args.point1, args.point2, args.num_lines_per_sample,
+        args.line_resolution, args.timesteps_to_sample, args.output_dir)
 
 
 if __name__ == "__main__":
